44caa3474719853666de461cb846b3705f6f09dc
AaronSirico-Columbia <144375285+AaronSirico-Columbia@users.noreply.github.com>
2023-09-13 23:23:53 -0500

    Add files via upload

---
+++ CaveOfEnviorments/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CaveOfEnviorments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            game.welcome();
+            game.OutputName();
+            game.GameLoop();
+        }
+    }
+}
